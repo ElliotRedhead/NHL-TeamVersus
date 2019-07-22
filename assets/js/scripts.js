6cafc91c8902c10926268b98282b831bdba8c403
unknown <elliot.redhead@gmail.com>
2019-07-22 18:53:36 +0100

    Attempted to console log the variables formed within the function outside of this.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,21 +3,20 @@ var secondTeamNumber = 2 //To be changed to variable input based on team selecto
 var teamUrl1 = 'https://statsapi.web.nhl.com/api/v1/teams/'+firstTeamNumber+'/stats'
 var teamUrl2 = 'https://statsapi.web.nhl.com/api/v1/teams/'+secondTeamNumber+'/stats'
 
+console.log('fetching data from NHL API...')
 
-console.log('fetching...')
-
-
-function dataRequest(teamSpecificUrl) {
-    fetch(teamSpecificUrl)
+async function dataRequest(teamSpecificUrl) {
+    let data = await fetch(teamSpecificUrl)
     .then(response => response.json())
     .then(function(data){
-        teamInfo = (data.stats[0].splits[0].team);
-        statisticsSingleSeason = (data.stats[0].splits[0].stat);
-        console.log(`${teamInfo.name}`);
-        console.log(`${statisticsSingleSeason.wins} wins`)
-        }
-        )
-}
+        let teamInfo = (data.stats[0].splits[0].team);
+        let statisticsSingleSeason = (data.stats[0].splits[0].stat);
+        return teamInfo, statisticsSingleSeason
+        });
+    };
 
-dataRequest(teamUrl1);
-dataRequest(teamUrl2);
+var a = dataRequest(teamUrl1);
+var b = dataRequest(teamUrl2);
+
+console.log(a.teamInfo, a.statisticsSingleSeason);
+console.log(b.teamInfo, b.statisticsSingleSeason);
