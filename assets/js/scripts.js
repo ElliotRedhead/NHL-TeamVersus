5f8b61997b5f87344025e8ee52431fe58c95a2e7
unknown <elliot.redhead@gmail.com>
2019-07-22 12:39:30 +0100

    Accessed the NHL API, wrote basic stats for a single team to HTML.

---
+++ assets/js/scripts.js
@@ -0,0 +1,18 @@
+const url = 'https://statsapi.web.nhl.com/api/v1/teams/5/stats'
+
+console.log('fetching...')
+
+fetch(url)
+    .then(response => response.json()) 
+    .then(function(data){
+        return data.stats[0].splits[0];
+    })
+    .then(function(statistics){
+        console.log(statistics)
+        document.getElementById("teamname").innerHTML = `Team: ${statistics.team.name}`
+        document.getElementById("wins").innerHTML = `Wins: ${statistics.stat.wins}`
+        document.getElementById("losses").innerHTML = `Losses: ${statistics.stat.losses}`
+    })
+
+        
+
