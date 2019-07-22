741c5caf62d21823b63a4fffa7c8f561382bf985
unknown <elliot.redhead@gmail.com>
2019-07-22 14:30:38 +0100

    Attempted comparison of statistics within JS, assigned these values to separate list items in HTML.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,21 +1,43 @@
-const url = 'https://statsapi.web.nhl.com/api/v1/teams/5/stats'
+var firstTeamNumber = 1
+var secondTeamNumber = 2
+var url1 = 'https://statsapi.web.nhl.com/api/v1/teams/'+firstTeamNumber+'/stats'
+var url2 = 'https://statsapi.web.nhl.com/api/v1/teams/'+secondTeamNumber+'/stats'
 
 console.log('fetching...')
 
-fetch(url)
+fetch(url1)
     .then(response => response.json()) 
     .then(function(data){
         return data.stats[0].splits[0];
     })
-    .then(function(statistics){
-        console.log(statistics)
-        document.getElementById("teamname").innerHTML = `Team: ${statistics.team.name}`
-        document.getElementById("wins").innerHTML = `Wins: ${statistics.stat.wins}`
-        if (statistics.stat.wins > 30) {
-            document.getElementById("wins").style.color="green";
+    .then(function(statistics1){
+        console.log(statistics1)
+        document.getElementById("firstteamname").innerHTML = `Team: ${statistics1.team.name}`
+        document.getElementById("firstwins").innerHTML = `Wins: ${statistics1.stat.wins}`
+        if (statistics1.stat.wins > statistics2.stat.wins) {
+            document.getElementById("firstwins").style.color="green";
         }
-        document.getElementById("losses").innerHTML = `Losses: ${statistics.stat.losses}`
-    })
+        document.getElementById("firstlosses").innerHTML = `Losses: ${statistics1.stat.losses}`
+        return(statistics1)
+    }
+    )
 
+
+fetch(url2)
+    .then(response => response.json()) 
+    .then(function(data){
+        return data.stats[0].splits[0];
+    })
+    .then(function(statistics2){
+        console.log(statistics2)
+        document.getElementById("secondteamname").innerHTML = `Team: ${statistics2.team.name}`
+        document.getElementById("secondwins").innerHTML = `Wins: ${statistics2.stat.wins}`
+        if (statistics2.stat.wins > statistics1.stat.wins) {
+            document.getElementById("secondwins").style.color="green";
+        }
+        document.getElementById("secondlosses").innerHTML = `Losses: ${statistics2.stat.losses}`
+        return (statistics2)
+    }
+    )
         
 
