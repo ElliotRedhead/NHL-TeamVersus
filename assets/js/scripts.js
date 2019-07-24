e5669837fe4a768c19ce8517a6cf1d2a80355ff9
unknown <elliot.redhead@gmail.com>
2019-07-24 17:52:01 +0100

    Removed superfluous code.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,30 +1,3 @@
-// var firstTeamNumber = 1 //To be changed to variable input based on team selector.
-// var secondTeamNumber = 2 //To be changed to variable input based on team selector.
-// var teamUrl1 = 'https://statsapi.web.nhl.com/api/v1/teams/'+firstTeamNumber+'/stats'
-// var teamUrl2 = 'https://statsapi.web.nhl.com/api/v1/teams/'+secondTeamNumber+'/stats'
-// var teamInfo;
-// var statisticsSingleSeason;
-
-// console.log('fetching data from NHL API...')
-
-// async function dataRequest(teamSpecificUrl) {
-//     let data = await fetch(teamSpecificUrl)
-//     .then(response => response.json())
-//     .then(function(data){
-//         var teamInfo = (data.stats[0].splits[0].team);
-//         var statisticsSingleSeason = (data.stats[0].splits[0].stat);
-//         return teamInfo, statisticsSingleSeason;
-//         }
-//         );
-//     };
-
-
-// dataRequest(teamUrl1);
-// console.log(teamInfo, statisticsSingleSeason);
-
-// dataRequest(teamUrl2);
-// console.log(teamInfo, statisticsSingleSeason);
-
 const url = 'https://statsapi.web.nhl.com/api/v1/teams/';
 
 function dataRequest(nhlUrl, teamNumber) {
