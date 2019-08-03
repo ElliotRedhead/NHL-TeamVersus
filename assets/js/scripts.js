15e38148efcb67541b4ae24edfd5a0e84409a1a4
unknown <elliot.redhead@gmail.com>
2019-08-03 16:45:59 +0100

    Added a function to request each team's statistics, beautified code.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,37 +3,40 @@ const fetchUrl = 'https://statsapi.web.nhl.com/api/v1/teams/';
 function getTeamName() {
     const teamName1 = document.getElementById("teamSelect1").value;
     const teamName2 = document.getElementById("teamSelect2").value;
-    dataRequest(teamName1,teamName2)
+    dataRequest(teamName1, teamName2)
 }
 
 
-function dataRequest(teamName1,teamName2){
-    const fetchUrl = 'https://statsapi.web.nhl.com/api/v1/teams/';
+function dataRequest(teamName1, teamName2) {
     fetch(fetchUrl)
-    .then(res => res.json())
-    .then(data => {
-    getTeamID(data, teamName1, teamName2)
-    });
+        .then(res => res.json())
+        .then(data => {
+            getTeamID(data.teams, teamName1, teamName2)
+        });
 }
 
-function getTeamID(dataset,teamName1, teamName2) {
+function getTeamID(dataset, teamName1, teamName2) {
     var targetTeam1;
     var targetTeam2;
-    targetTeam1 = dataset.teams.find(team1 => team1.name==teamName1);
-    targetTeam2 = dataset.teams.find(team2 => team2.name==teamName2);
+    targetTeam1 = dataset.find(team1 => team1.name == teamName1);
+    targetTeam2 = dataset.find(team2 => team2.name == teamName2);
     var teamID1 = targetTeam1.id;
     var teamID2 = targetTeam2.id;
-    console.log(teamID1);
-    console.log(teamID2);
+    statsRequest(teamID1, teamID2)
 }
 
 
-function statsRequest(fetchUrl, teamNumber) {
-  fetch(fetchUrl + teamNumber + '/stats')
-    .then(res => res.json())
-    .then(data => {
-        printStats(data);
-    });
+function statsRequest(teamID1, teamID2) {
+    fetch(fetchUrl + teamID1 + '/stats')
+        .then(res => res.json())
+        .then(statSet1 => {
+            const teamStat1 = (statSet1.stats[0].splits[0].stat)
+        });
+    fetch(fetchUrl + teamID2 + '/stats')
+        .then(res => res.json())
+        .then(statSet2 => {
+            const teamStat2 = (statSet1.stats[0].splits[0].stat)
+        });
 }
 
 
@@ -43,7 +46,3 @@ function printStats(dataset) {
     console.log(wins);
 }
 
-
-getTeamName()
-//statsRequest(url, 1);
-//statsRequest(url, 2);
